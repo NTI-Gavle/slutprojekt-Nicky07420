33e7f82b1e1aeaf688541dc0bf49da729c318e46
nicky07420 <nickyalexander.paatalo@elev.ga.ntig.se>
2026-03-06 10:59:25 +0100

    tested changing colours, this seems good

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@ $pageTitle = "Home"; // <-- set dynamic page title
 require_once __DIR__ . '/../includes/header.php';
 ?>
 
-<h2>Welcome to My Home Project</h2>
+<h2>Suspiciously unsuspicious-looking questionably ordinary posting website</h2>
 <p>This is the homepage.</p>
 
 <?php
-require_once __DIR__ . '/../includes/footer.php';
+require_once __DIR__ . '/../includes/footer.php';   
